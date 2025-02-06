5ab6364d87a00f6301669326fc3b07cd5ff7b40b
Ivveh83 <ivar.hogblom@hotmail.com>
2025-02-06 01:39:24 +0100

    Second commit. Add bootstrap template. Add my name and title as pics. Add background pic. Fixing style and other cool stuff.

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/store')
+def store():
+    return render_template('store.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
